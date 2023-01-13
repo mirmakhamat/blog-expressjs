43c1edc3ac0eb67d97a3b1458951898aa8d89b83
mr-sunnatov <sunnatovm01@gmail.com>
2023-01-13 17:33:29 +0500

    fixed

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 
+const http = require('http');
+const config = require('config');
+const mongoose = require('mongoose');
+
 const router = require('./router.js');
 
 const app = express();
@@ -12,14 +16,28 @@ app.use(fileUpload({}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+const port = config.get('serverPort') || 3000
+app.set('port', port);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 app.use(router)
 
+const server = http.createServer(app);
 
-module.exports = app;
+const start = async () => {
+  try {
+    await mongoose.set('strictQuery', true);
+    await mongoose.connect(config.get('MONGODB_URI'))
+    server.listen(port);
+    console.log(`Server ${port} da ishga tushdi`)
+  } catch (e) {
+    console.error('error', e)
+  }
+}
+
+start();
